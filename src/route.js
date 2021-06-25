2fc617c2e5bb1b76c03c8e5acaac0609f4b7f8bd
Pedro Gutieres <pedro-gabriel1@hotmail.com>
2021-06-25 12:32:47 -0300

    criado controller para informacoes no back e middleware no server para gerenciar infos passadas pelo metodo formulario metodo post

--- src/route.js
+++ src/route.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const QuestionController = require("./controllers/QuestionController");
 
 const route = express.Router();
 
@@ -7,6 +8,6 @@ route.get("/room", (req, res) => res.render("room"));
 route.get("/create-pass", (req, res) => res.render("create-pass"));
 
 //Formato que o formulario de dentro da modal tem que passar a informacao
-// route.post("/room/:room/:question/:action");
+route.post("/room/:room/:question/:action", QuestionController.index);
 
 module.exports = route;
