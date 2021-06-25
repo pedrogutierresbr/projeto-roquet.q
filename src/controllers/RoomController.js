0ba4ae066284ef597e343bf6642322fe84b44521
Pedro Gutieres <pedro-gabriel1@hotmail.com>
2021-06-25 17:28:40 -0300

    criado funcao para gerar id de forma aleatoria e criado funcao para salvar id e password no db

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -1,6 +1,24 @@
+const Database = require("../db/config");
+
 module.exports = {
-    create(req, res) {
-        let roomId = 123456;
+    async create(req, res) {
+        const db = await Database();
+        let roomId = "";
+        const pass = req.body.password;
+
+        for (let i = 0; i < 6; i++) {
+            roomId += Math.floor(Math.random() * 10).toString();
+        }
+
+        await db.run(`INSERT INTO rooms (
+            id,
+            pass
+        ) VALUES (
+            ${parseInt(roomId)},
+            ${pass}
+        )`);
+
+        await db.close(``);
 
         res.redirect(`/room/${roomId}`);
     },
