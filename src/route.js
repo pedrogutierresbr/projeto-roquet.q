41aa2540e4abb0c9e3c64ad853da3fa96bf9f308
Pedro Gutieres <pedro-gabriel1@hotmail.com>
2021-06-27 14:50:41 -0300

    criado pagina e condicional no roomcontroller pra quando tentar entrar em sala nao existente

--- src/route.js
+++ src/route.js
@@ -6,6 +6,7 @@ const route = express.Router();
 
 route.get("/", (req, res) => res.render("index", { page: "enter-room" }));
 route.get("/create-pass", (req, res) => res.render("index", { page: "create-pass" }));
+route.get("/non-existent", (req, res) => res.render("index", { page: "non-existent" }));
 
 route.post("/create-room", RoomController.create);
 route.get("/room/:room", RoomController.open);
